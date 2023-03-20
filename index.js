bf1f1f23ef936d58efd393d817fb11cf77803f02
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-03-19 20:45:14 -0400

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(event = "roller-skate"){
+    return `This Saturday, I want to ${event}!`;
+}
+function mondayWork(event = "go to the office"){
+    return `This Monday, I will ${event}.`
+}
+function wrapAdjective(flair = "special"){
+    return function(string = "a dedicated programmer"){
+        return `You are ${flair}${string}${ flair}!`
+    }
+}
